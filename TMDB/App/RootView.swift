2546dc39d64f2fc1b9e5bd25cd7bf92b696896a5
vinothvino42 <vinothvino42@gmail.com>
2023-09-28 13:19:13 +0530

    feat: add home tab view and handle login, logout and onboarding state in root view

--- TMDB/App/RootView.swift
+++ TMDB/App/RootView.swift
@@ -2,66 +2,36 @@
 //  RootView.swift
 //  TMDB
 //
-//  Created by Vinoth Vino on 18/09/23.
+//  Created by Vinoth Vino on 28/09/23.
 //
 
 import SwiftUI
 
-enum ThemeMode: Int, CaseIterable {
-    case system
-    case lightMode
-    case darkMode
-}
-
 struct RootView: View {
-    @State private var isLoggedIn = false
-    @AppStorage("themeMode") private var themeMode: Int = ThemeMode.allCases.first!.rawValue
-    @Environment(\.colorScheme) private var colorScheme
-    
-    private var selectedColorScheme: ColorScheme? {
-        guard let themeMode = ThemeMode(rawValue: themeMode) else { return nil }
-        
-        switch themeMode {
-        case .lightMode:
-            return .light
-        case .darkMode:
-            return .dark
-        default:
-            return nil
-        }
-    }
+    @StateObject private var session = SessionManager()
     
     var body: some View {
         ZStack {
-            if (isLoggedIn) {
-                NavigationStack {
-                    TabView {
-                        HomeView()
-                            .tabItem {
-                                Label("Home", systemImage: "house")
-                            }
-                        
-                        SearchView()
-                            .tabItem {
-                                Label("Search", systemImage: "magnifyingglass")
-                            }
-                        
-                        SettingsView()
-                            .tabItem {
-                                Label("Settings", systemImage: "gearshape")
-                            }
-                    }
-                }
-            } else {
+            switch session.currentState {
+            case .loggedIn:
+                HomeTabView()
+                    .environmentObject(session)
+                    .transition(.opacity)
+            case .loggedOut:
                 LoginView()
+                    .environmentObject(session)
+                    .transition(.opacity)
+            default:
+                OnboardView()
+                    .environmentObject(session)
+                    .transition(.asymmetric(insertion: .opacity, removal: .move(edge: .leading)))
             }
         }
-        .preferredColorScheme(selectedColorScheme)
+        .animation(.easeInOut, value: session.currentState)
+        .onAppear(perform: session.configureInitialState)
     }
 }
 
-struct RootView_Previews: PreviewProvider {
-    static var previews: some View {
-        RootView()
-    }
+#Preview {
+    RootView()
 }
---
+++ TMDB/Views/HomeTabView/HomeTabView.swift
@@ -0,0 +1,63 @@
+//
+//  HomeTabView.swift
+//  TMDB
+//
+//  Created by Vinoth Vino on 18/09/23.
+//
+
+import SwiftUI
+
+enum ThemeMode: Int, CaseIterable {
+    case system
+    case lightMode
+    case darkMode
+}
+
+struct HomeTabView: View {
+    @State private var isLoggedIn = false
+    @AppStorage("themeMode") private var themeMode: Int = ThemeMode.allCases.first!.rawValue
+    @Environment(\.colorScheme) private var colorScheme
+    
+    private var selectedColorScheme: ColorScheme? {
+        guard let themeMode = ThemeMode(rawValue: themeMode) else { return nil }
+        
+        switch themeMode {
+        case .lightMode:
+            return .light
+        case .darkMode:
+            return .dark
+        default:
+            return nil
+        }
+    }
+    
+    var body: some View {
+        ZStack {
+            NavigationStack {
+                TabView {
+                    HomeView()
+                        .tabItem {
+                            Label("Home", systemImage: "house")
+                        }
+                    
+                    SearchView()
+                        .tabItem {
+                            Label("Search", systemImage: "magnifyingglass")
+                        }
+                    
+                    SettingsView()
+                        .tabItem {
+                            Label("Settings", systemImage: "gearshape")
+                        }
+                }
+            }
+        }
+        .preferredColorScheme(selectedColorScheme)
+    }
+}
+
+struct HomeTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeTabView()
+    }
+}
