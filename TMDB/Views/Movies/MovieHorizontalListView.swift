5e2cd76bf8c75ac8b870278b1f0bdf72f2193c66
vinothvino42 <vinothvino42@gmail.com>
2023-10-05 20:14:59 +0530

    feat: add movie horizontal list view

---
+++ TMDB/Views/Movies/MovieHorizontalListView.swift
@@ -0,0 +1,44 @@
+//
+//  MovieHorizontalListView.swift
+//  TMDB
+//
+//  Created by Vinoth Vino on 18/09/23.
+//
+
+import SwiftUI
+import NukeUI
+
+struct MovieHorizontalListView: View {
+    let title: String
+    let isLoading: Bool
+    let movies: [Movie]?
+    
+    var body: some View {
+        if movies == nil || movies!.isEmpty {
+            MovieListLoaderView()
+        } else {
+            VStack(alignment: .leading, spacing: 10) {
+                Text(title + " Movies")
+                    .font(.headline)
+                    .fontWeight(.bold)
+                ScrollView(.horizontal) {
+                    LazyHStack(spacing: 8) {
+                        ForEach(movies!, id: \.self) { movie in
+                            MovieCard(movie: movie)
+                        }
+                    }
+                }
+                .scrollIndicators(.hidden)
+                .scrollDisabled(movies == nil || movies!.isEmpty)
+            }
+            .padding(.horizontal)
+            .padding(.bottom, 20)
+        }
+    }
+}
+
+struct MovieListView_Previews: PreviewProvider {
+    static var previews: some View {
+        MovieHorizontalListView(title: "Trending", isLoading: false, movies: [])
+    }
+}
